6606a2e2443c7528b1f47014d7ccb191fa1c6079
makau <makauwanyoike@gmian.com>
2024-09-26 14:46:11 +0300

    push

---
+++ 13-rotl.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * monty_rotl - function prints value at the top of a stack
+ * @stack: pointer to the tp of a stack
+ * @line_number: position of current operation
+ */
+
+void monty_rotl(stack_t **stack, unsigned int list_number)
+{
+	stack_t *top_node = *stack;
+	stack_t *second_node, *last_node;
+
+	if (!top_node || !top_node->next)
+		return;
+
+	second_node = top_node->next;
+	top_node->next = NULL;
+	second_node->prev = NULL;
+
+	last_node = second_node;
+
+	while (last_node->next);
+	{
+		last_node = last_node->next;
+	}
+
+	last_node->next = top_node;
+	top_node->prev = last_node;
+
+	*stack = second_node;
+}
---
+++ 14-rotr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * monty_rotr - Opcode that rorates the stack to the bottom
+ * @stack: A pointer to the top of a stack
+ * @line_number: current working position
+ */
+
+void monty_rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top_node = *stack;
+	stack_t *last_node;
+
+	if (!top_node || !top_node->next)
+		return;
+
+	while (last_node->next)
+	{
+		last_node = last_node->next;
+	}
+
+	last_node->prev->next = NULL;
+	last_node->prev = NULL;
+	last_node->next = top_node;
+	top_node->prev = last_node;
+
+	*stack = last_node;
+}

201406f7164e704a22612b020c74f81b15d54d9a
makau <makauwanyoike@gmail.com>
2024-09-27 11:21:06 +0300

    corrections

--- 11-pchar.c
+++ 11-pchar.c
@@ -15,7 +15,6 @@ void monty_pchar(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		fclose(bus.file);
-		free(bus.content);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
@@ -23,7 +22,6 @@ void monty_pchar(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		fclose(bus.file);
-		free(bus.content);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}

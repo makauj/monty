ee16d13dd45ed9acd89b4b90df1e6c66d528bb3c
makau <makauwanyoike@gmail.com>
2024-09-27 08:09:25 +0300

    corrections

--- test.m
+++ test.m
@@ -1,23 +1,62 @@
-# Sample Monty bytecode file
-
-push 5      # Pushes 5 onto the stack
-push 10     # Pushes 10 onto the stack
-push 3      # Pushes 3 onto the stack
-pall        # Prints all elements (3, 10, 5)
-add         # Adds top two elements (3 + 10 = 13), new top: 13
-pint        # Prints the top element (13)
-push 8      # Pushes 8 onto the stack
+push 5
+push 10
 pall
-sub         # Subtracts top two elements (13 - 8 = 5), new top: 5
-pall        # Prints all elements (5, 5)
-push 2      # Pushes 2 onto the stack
-mul         # Multiplies top two elements (5 * 2 = 10), new top: 10
-pall        # Prints all elements (10, 5)
-push 4      # Pushes 4 onto the stack
-div         # Divides second top element by the top (10 / 4 = 2), new top: 2
-pall        # Prints all elements (2, 5)
-push 7      # Pushes 7 onto the stack
-mod         # Modulo of second top element by top (5 % 7 = 5), new top: 5
-pall        # Prints all elements (5, 2)
-pchar       # Prints the char at the top of the stack (ASCII 5 -> Error)
-nop         # Does nothing
+push 42
+pint
+push 1
+push 2
+pop
+pall
+push 1
+push 2
+swap
+pall
+push 3
+push 5
+add
+pall
+push 10
+push 5
+sub
+pall
+push 10
+push 2
+div
+pall
+push 2
+push 3
+mul
+pall
+push 10
+push 3
+mod
+pall
+push 65
+pchar
+push 72
+push 101
+push 108
+push 108
+push 111
+push 0
+pstr
+push 1
+push 2
+push 3
+rotl
+pall
+push 1
+push 2
+push 3
+rotr
+pall
+stack
+push 1
+push 2
+pall
+queue
+push 1
+push 2
+pall
+push 1
+push two

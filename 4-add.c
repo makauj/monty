3e1a72c9e0db5c9064719494aabcffd54e6c91f5
makau <makauwanyoike@gmail.com>
2024-09-27 11:14:06 +0300

    corrections

--- 4-add.c
+++ 4-add.c
@@ -20,7 +20,6 @@ void monty_add(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		fclose(bus.file);
-		free(bus.content);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}

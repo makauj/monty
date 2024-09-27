5d091bf28555fae153ad3523293679fe5b6a9cba
makau <makauwanyoike@gmail.com>
2024-09-27 11:20:19 +0300

    corrections

--- 9-mod.c
+++ 9-mod.c
@@ -29,7 +29,6 @@ void monty_mod(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
 		fclose(bus.file);
-		free(bus.content);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}

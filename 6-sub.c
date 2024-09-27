b675a5ef1593cc44e6b05fd051f9cc1c770dd6d7
makau <makauwanyoike@gmail.com>
2024-09-27 11:15:07 +0300

    corrections

--- 6-sub.c
+++ 6-sub.c
@@ -18,7 +18,6 @@ void monty_sub(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		fclose(bus.file);
-		free(bus.content);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}

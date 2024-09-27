06f529c93db98ae117f5a06792b9c78f15f4f40f
makau <makauwanyoike@gmail.com>
2024-09-27 08:29:04 +0300

    corrections

--- 0-push.c
+++ 0-push.c
@@ -21,13 +21,11 @@ void monty_push(stack_t **stack, unsigned int line_number)
 		if (flag == 1)
 		{ fprintf(stderr, "L%d: usage: push integer\n", line_number);
 			fclose(bus.file);
-			free(bus.content);
 			free_stack(*stack);
 			exit(EXIT_FAILURE); }}
 	else
 	{ fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		fclose(bus.file);
-		free(bus.content);
 		free_stack(*stack);
 		exit(EXIT_FAILURE); }
 	n = atoi(bus.arg);
---
+++ test1.m
@@ -0,0 +1,7 @@
+push 1
+push 2
+push 2
+pop
+pall
+push two
+pall

70b6279a6d40f6e885444fea123c7fd82d68d9c6
makau <makauwanyoike@gmail.com>
2024-09-27 11:15:32 +0300

    corrections

--- 7-div.c
+++ 7-div.c
@@ -21,7 +21,6 @@ void monty_div(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		fclose(bus.file);
-		free(bus.content);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
